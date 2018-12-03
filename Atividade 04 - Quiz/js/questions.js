76e8b05680cf157fcb206b4a473a1885e5ab04cf
Kelvyn Pereira <kelvynjpereira@hotmail.com>
2018-12-03 15:22:13 -0200

    Upload

---
+++ Atividade 04 - Quiz/js/questions.js
@@ -0,0 +1,26 @@
+var questions = [{
+  question: "Qual seria a origem do nome do bairro? ",
+  choices: ['Originado da primeira tribo índigena ao ocupar o local, os "Macaxeiras."', 'Devido a uma grande plantação de Macaxeiras na região.', 'Tem origem da "feira de alimentos." antes presente no local, onde o principal produto de venda era a Macaxeira.', 'A origem é desconhecida.'],
+  correctAnswer: 1,
+  answerLocation: '"linkaqui1"'
+}, {
+  question: "Quem comprou a Fábrica de Tecidos Apipucos, localizada no bairro, em 1925?",
+  choices: ['Guimarães Rosa.','Joaquim Nambuco.','Othon Bezerra de Melo.','Franscio Freitas Santos.'],
+  correctAnswer: 2,
+  answerLocation: '"linkaqui2"'
+}, {
+  question: "Qual teria sido o(s) motivo(s) para falência da Fábrica de Tecidos, após a morte do seu fundador?",
+  choices: ['Crise no setor de algodão.', 'Altos impostos.', 'Desinteresse dos herdeiros.', 'Todas as alternativas estão corretas.'],
+  correctAnswer: 3,
+  answerLocation: '"linkaqui3"'
+}, {
+  question: "No ano de 2012, iniciou-se a construção de ...... , no antigo local da fábrica.",
+  choices: ['uma Escola', 'um Campo de futebol', 'um Estacionamento', 'uma Escola e Parque'],
+  correctAnswer: 3,
+  answerLocation: '"linkaqui4"'
+}, {
+  question: "O bairro é cortado por qual grande avenida de Recife?",
+  choices: ['Av Conselheiro Aguiar.', 'Av. Presidente Kennedy.', 'Av. Norte', 'Av. Mascarenhas de Morais.'],
+  correctAnswer: 2,
+  answerLocation: '"linkaqui5"'
+}];
