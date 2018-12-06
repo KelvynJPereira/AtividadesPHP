5c3138156cfbb2dc95f81e03a3802f92803a67d9
Kelvyn <Kelvynjpereira@hotmail.com>
2018-12-06 00:55:35 -0200

    Update teste_conexao.php

--- Atividade 08 - Listagem de dados/teste_conexao.php
+++ Atividade 08 - Listagem de dados/teste_conexao.php
@@ -2,7 +2,7 @@
 
 // Criando conexão
 try { 
-	$conn = new PDO("mysql:host=localhost;dbname=bd_pdo", "root","root");
+	$conn = new PDO("mysql:host=localhost;dbname=bd_etex", "root","root");
 	} catch (PDOException $e) {
 		echo "<h1>Erro na conexão... </h1>".$e;	
 	}
